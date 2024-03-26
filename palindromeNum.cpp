452314c0a47889cb5308da14827c35cd9bdb4fe2
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-03-27 01:16:21 +0530

    in cpp palindrome Number

---
+++ palindromeNum.cpp
@@ -0,0 +1,13 @@
+
+    class Solution {
+        public:
+            bool isPalindrome(int x) {
+                string s=to_string(x);
+                string p=s;
+                reverse(p.begin(), p.end());
+                if(p==s)return true;
+                else return false;
+             
+            }
+        };
+
