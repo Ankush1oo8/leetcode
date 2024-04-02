75375a12265234a8b9a9616478a36adad216a9a7
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-03 01:22:46 +0530

    136. Single Number

---
+++ singleNumber.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class singleNumber {
+      public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+       for(int i=0;i<nums.length;i++){
+         if(map.get(nums[i])==1){
+                return nums[i];
+            }
+       }
+        return nums[0];
+    }
+}
