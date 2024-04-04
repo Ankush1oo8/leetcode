86817f0a9c152b1f5cd25805097a3666cde87c24
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-04 15:22:29 +0530

    1614. Maximum Nesting Depth of the Parentheses

---
+++ maxNestingDepthofParentheses.java
@@ -0,0 +1,16 @@
+class maxNestingDepthofParentheses {
+    public int maxDepth(String s) {
+        int open=0;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                open++;
+            }
+            if(s.charAt(i)==')'){
+                open--;
+            }
+            max=Math.max(max,open);
+        }
+        return max;
+    }
+}
