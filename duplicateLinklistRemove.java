08488e07495592e2b794e526096f868f354f867a
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-04 15:33:45 +0530

    duplicate linklist

--- duplicateLinklistRemove.java
+++ duplicateLinklistRemove.java
@@ -1,4 +1,12 @@
 public class duplicateLinklistRemove {
+     public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
+        
     public ListNode deleteDuplicates(ListNode head) {
         ListNode curr=head;
         while(curr!=null){
