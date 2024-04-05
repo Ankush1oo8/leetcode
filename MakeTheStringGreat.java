17a7a8b4cf858c51ea041ea636cd5714c8fac8a1
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-05 15:59:17 +0530

    1544. Make The String Great

---
+++ MakeTheStringGreat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MakeTheStringGreat {
+    public String makeGood(String s) {
+        StringBuilder ans=new StringBuilder();
+        for(char ch : s.toCharArray()){
+            int len=ans.length();
+            if(len>0 && Math.abs((ans.charAt(len-1)-ch))==32){
+                ans.deleteCharAt(len-1);
+            }else{
+                ans.append(ch);
+            }        
+        }
+        return ans.toString();
+    }
+}
