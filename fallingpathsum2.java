c15219ee725070947b1184d12c2e6b1244d561e7
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-26 16:29:21 +0530

    1289. Minimum Falling Path Sum II

---
+++ fallingpathsum2.java
@@ -0,0 +1,33 @@
+public class fallingpathsum2 {
+    
+    
+        public int minFallingPathSum(int[][] grid) {
+            int firstMin=0;
+            int secondMin=0;
+            int firstMinPos=-1;
+            int INF=Integer.MAX_VALUE;
+            for(int []row:grid){
+                int curFirstMin=INF;
+                int curSecondMin=INF;
+                int curFirstMinPos=-1;
+    
+                for(int j=0;j<row.length;j++){
+                    int sum=(j!=firstMinPos ?firstMin:secondMin)+row[j];
+    
+                    if(sum<curFirstMin){
+                        curSecondMin=curFirstMin;
+                        curFirstMin=sum;
+                        curFirstMinPos=j;
+                    }else if(sum<curSecondMin){
+                        curSecondMin=sum;
+                    }
+                }
+                firstMin=curFirstMin;
+                secondMin=curSecondMin;
+                firstMinPos=curFirstMinPos;
+            }
+            return firstMin;
+        }
+    
+
+}
