e5b00899fbe2d2bccfb9525031f24eca354387e2
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-04-28 02:35:03 +0530

    268. Missing Number

---
+++ missingNumber.java
@@ -0,0 +1,11 @@
+public class missingNumber {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=(n*(n+1))/2;
+        int sum1=0;
+        for(int i=0;i<nums.length;i++){
+            sum1+=nums[i];
+        }
+        return sum-sum1;
+    }
+}
