c891693146602757131c91435e28962843b9552f
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-05-17 12:23:28 +0530

    1325. Delete Leaves With a Given Value

---
+++ removeTargetLeaf.java
@@ -0,0 +1,15 @@
+class removeTargetLeaf{
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null) return null;
+      
+      root.left = removeLeafNodes(root.left, target);
+      root.right = removeLeafNodes(root.right, target);
+      
+      if(root.left == null & root.right == null){
+          if(root.val == target) return null;
+          else return root;
+      }
+      
+      return root;
+  }
+}
