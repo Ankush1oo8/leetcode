4d78acec76c8994c875e84d34427b9fba0078b54
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-05-25 18:56:42 +0530

    140. Word Break II

---
+++ wordBreak2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class wordBreak2 {
+    
+  int n;
+  Set<String> dict = new HashSet<>();
+  List<String> res = new ArrayList<>();
+
+  private void backTrack(String s, int i, String l) {
+    if (i == n) {
+      res.add(l.substring(0, l.length() - 1));
+      return;
+    }
+    for (var j=i; j<n; j++) {
+      var sub = s.substring(i, j+1);
+      if (!dict.contains(sub)) continue;
+
+      backTrack(s, j+1, l + sub + " ");
+    }
+  }
+
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    n = s.length();
+    for (var word : wordDict) dict.add(word);
+
+    backTrack(s, 0, "");
+
+    return res;
+  }
+}
+
