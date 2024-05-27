ceac2bd604f731a40064d92642d5d7ad807fe0de
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-05-27 12:58:51 +0530

    455. Assign Cookies

---
+++ AssignCookies.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class AssignCookies {
+     public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int l=0,r=0;
+        while(l<m && r<n){
+            if(g[r]<=s[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+}
