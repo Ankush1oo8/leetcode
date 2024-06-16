1d0ee86dc8e0d7d500b51fdfeeb2d50e1263c3b7
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-06-16 11:51:36 +0530

    330. Patching Array

---
+++ patchingArray.java
@@ -0,0 +1,16 @@
+public class patchingArray {
+    public int minPatches(int[] nums, int n) {
+        int ans=0,i=0;
+        long miss=1;
+        while(miss<=n){
+            if(i<nums.length && nums[i]<=miss){
+                miss+=nums[i];
+                i++;
+            }else{
+                miss+=miss;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
