5b02c529a5e4596b4c0d3b5175390af0da73a8da
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-06-25 12:59:51 +0530

    1038. Binary Search Tree to Greater Sum Tree

--- BST2GST.java
+++ BST2GST.java
@@ -1,30 +1,29 @@
 import javax.swing.tree.TreeNode;
 
-public class BST2GST {
-     public TreeNode convertBST(TreeNode root) {
-        TreeNode cur = root;
-        int sum = 0;
-        while (cur != null) {
-            if (cur.right != null) { // traverse right subtree.
-                TreeNode leftMost = cur.right;
-                while (leftMost.left != null && leftMost.left != cur) { // locate the left-most node of cur's right subtree.
-                    leftMost = leftMost.left;
-                }
-                if (leftMost.left == null) { // never visit the left-most node yet.
-                    leftMost.left = cur; // construct a way back to cur.
-                    cur = cur.right; // explore right.
-                }else { // visited leftMost already, which implies now on way back.
-                    leftMost.left = null; // cut off the fabricated link.
-                    sum += cur.val; // update sum.
-                    cur.val = sum; // update node value.
-                    cur = cur.left; // continue on way back.
-                }
-            }else { // no right child: 1) cur is the right-most of unvisited nodes; 2) must traverse left.
-                sum += cur.val; // update sum.
-                cur.val = sum; // update node value.
-                cur = cur.left; // continue on way back.
+public TreeNode bstToGst(TreeNode root) {
+    int s = 0;
+    TreeNode node = root;
+    while (root != null) {
+        if (root.right == null) {
+            s += root.val;
+            root.val = s;
+            root = root.left;
+        } else {
+            TreeNode next = root.right;
+            while (next.left != null && next.left != root) {
+                next = next.left;
+            }
+            if (next.left == null) {
+                next.left = root;
+                root = root.right;
+            } else {
+                s += root.val;
+                root.val = s;
+                next.left = null;
+                root = root.left;
             }
         }
-        return root;
     }
+    return node;
+}
 }
