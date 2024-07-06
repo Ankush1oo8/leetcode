896d5856993ecd472519efb19c334d13288e65a8
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-07-06 13:37:59 +0530

    2582. Pass the Pillow

---
+++ passPillow.java
@@ -0,0 +1,8 @@
+class passPillow{
+    public int passThePillow(int n, int time) {
+    time %= (n - 1) * 2;
+    if (time < n) 
+        return 1 + time;
+    return n - (time - (n - 1)); 
+    }
+}
