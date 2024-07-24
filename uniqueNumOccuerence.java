105223f4c0ebc09a1678eac85c12934226ba299d
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-07-25 00:39:20 +0530

    1207. Unique Number of Occurrences

--- uniqueNumOccuerence.java
+++ uniqueNumOccuerence.java
@@ -3,16 +3,16 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class uniqueNumOccuerence {
-    
+
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i:arr){
-        map.put(i,map.getOrDefault(i,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-         HashSet<Integer> hashSet = new HashSet<>();
+        HashSet<Integer> hashSet = new HashSet<>();
         boolean flag = true;
-        for (Map.Entry<Integer, Integer> mapElement : map.entrySet()){
-            if (!hashSet.add(mapElement.getValue())){
+        for (Map.Entry<Integer, Integer> mapElement : map.entrySet()) {
+            if (!hashSet.add(mapElement.getValue())) {
                 flag = false;
             }
         }
